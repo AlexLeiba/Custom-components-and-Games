6b62a715376bffce8f9043570d8fe941713e0122
AlexLeiba <Alexleiba13@gmail.com>
2025-10-25 12:18:25 +0300

    alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      //an alias is the alternative name or shortcut/In this case for a path
+      "@": "/src", //the src folder will be the root starting with @ when importing something
+    },
+  },
 });
+
+// '@components': '/src/components',
+//     '@utils': '/src/utils'
