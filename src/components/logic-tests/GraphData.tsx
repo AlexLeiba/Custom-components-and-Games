87e77765f4e42c69c85f98d84907662c05363efe
AlexLeiba <Alexleiba13@gmail.com>
2025-12-29 17:40:42 +0200

    graph data handling

---
+++ src/components/logic-tests/GraphData.tsx
@@ -0,0 +1,188 @@
+import { useEffect, useState, type DragEvent, type MouseEvent } from "react";
+
+const initialNodes = [
+  { id: "A", x: 100, y: 100 },
+  { id: "B", x: 300, y: 100 },
+  { id: "C", x: 200, y: 250 },
+  { id: "D", x: 400, y: 250 },
+  { id: "E", x: 200, y: 400 },
+];
+
+const initialEdges = [
+  { source: "A", target: "B" },
+  { source: "A", target: "C" },
+];
+
+export function GraphData() {
+  const [nodes, setNodes] =
+    useState<{ id: string; x: number; y: number }[]>(initialNodes);
+  const [edges, setEdges] =
+    useState<{ source: string; target: string }[]>(initialEdges);
+  console.log("🚀 ~ GraphData ~ edges:", edges);
+
+  const [selectedNode, setSelectedNode] = useState<{
+    source: string;
+    target: string;
+  }>({
+    source: "",
+    target: "",
+  });
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const [draggingId, setDraggingId] = useState<string>("");
+
+  const getNode = (id: string) => nodes.find((n) => n.id === id);
+
+  function handleNodeClick(id: string) {
+    setSelectedNode((prev) => {
+      // if no source
+      if (!prev.source) {
+        return {
+          source: id,
+          target: "",
+        };
+      }
+
+      // if the same source
+      if (prev.source && prev.source === id) {
+        return {
+          source: "",
+          target: "",
+        };
+      }
+      // if source already exists
+      if (prev.source && prev.source !== id) {
+        return {
+          ...prev,
+          target: id, //set target
+        };
+      }
+
+      return prev;
+    });
+  }
+
+  useEffect(() => {
+    if (selectedNode.source && selectedNode.target) {
+      setEdges((prev) => [...prev, selectedNode]);
+    }
+  }, [selectedNode]);
+
+  // delete edge
+  function handleEdgeDelete(source: string, target: string) {
+    setEdges((prev) =>
+      prev.filter((edge) => edge.source !== source && edge.target !== target)
+    );
+  }
+
+  function handleDragNode(
+    node: { id: string; x: number; y: number },
+    e: DragEvent<SVGGElement>
+  ) {
+    console.log("🚀 ~ handleDragNode ~ e:", e);
+  }
+
+  function handleMouseMove(event: MouseEvent<SVGSVGElement>) {
+    if (!draggingId) return;
+    console.log("🚀 ~ handleMouseMove ~ event:", event);
+    const x = event.clientX; //new mouse position
+    const y = event.clientY;
+
+    // change position of each node element on move into svg view
+    // loop throug selected node and update its position
+
+    setNodes((prev) => {
+      return prev.map((node) => {
+        if (node.id === draggingId) {
+          return {
+            ...node,
+            x: x - offset.x,
+            y: y - offset.y,
+          };
+        }
+
+        return node;
+      });
+    });
+  }
+
+  function handleMouseDown(
+    event: MouseEvent<SVGGElement>,
+    node: { id: string; x: number; y: number }
+  ) {
+    console.log("🚀 ~ handleMouseDown ~ event:", event);
+    setDraggingId(node.id);
+
+    // // mouse position relative to node / to center position relative to node
+    setOffset({
+      x: event.clientX - node.x,
+      y: event.clientY - node.y,
+    });
+  }
+
+  function handleMouseUp() {
+    setDraggingId("");
+  }
+  return (
+    <svg
+      width="100%"
+      height="800"
+      style={{ border: "1px solid #ccc" }}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      {/* Edges */}
+      {edges.map((edge, i) => {
+        const source = getNode(edge.source);
+        const target = getNode(edge.target);
+
+        return (
+          <g key={i}>
+            <line
+              className="cursor-pointer"
+              onClick={() => handleEdgeDelete(edge.source, edge.target)}
+              key={i}
+              x1={source?.x} // x y of source node
+              y1={source?.y}
+              x2={target?.x} //x y of target node
+              y2={target?.y} //edge position for particular mapped edge
+              stroke="black" //edge color
+            />
+            {/* <text x={source?.x} y={source?.y} className="p-2 bg-red-600">
+              Delete
+            </text> */}
+          </g>
+        );
+      })}
+
+      {/* Nodes */}
+      {nodes.map((node) => (
+        <g
+          onDragStart={(e) => handleDragNode(node, e)}
+          onDrag={(e) => handleDragNode(node, e)}
+          key={node.id}
+          onClick={() => handleNodeClick(node.id)}
+          className="cursor-pointer"
+          onMouseDown={(e) => handleMouseDown(e, node)}
+        >
+          <circle
+            cx={node.x}
+            cy={node.y}
+            r={30}
+            fill={selectedNode.source === node.id ? "#46e5c5" : "#4f46e5"}
+            // fillOpacity={0.5}
+          ></circle>
+
+          <text
+            x={node.x}
+            y={node.y + 5}
+            textAnchor="middle"
+            fill="white"
+            fontSize="12"
+          >
+            {node.id}
+          </text>
+        </g>
+      ))}
+    </svg>
+  );
+}
--- src/pages/GridPage.tsx
+++ src/pages/GridPage.tsx
@@ -1,11 +1,13 @@
 import { TreeData } from "../components/logic-tests/TreeData";
 import { RxjsContainer } from "../components/logic-tests/RxjsContainer";
+import { GraphData } from "../components/logic-tests/GraphData";
 
 function GridPage() {
   return (
     <div>
-      <TreeData />
+      {/* <TreeData /> */}
       {/* <RxjsContainer /> */}
+      <GraphData />
     </div>
   );
 }
